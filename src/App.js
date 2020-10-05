3e84cffac6d20e84c7ad535f5576fe556118b7d1
MdTanvir-ICE <tanvir.pust.ice09@gmail.com>
2020-10-05 20:44:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AdminPanel from './Component/AdminPanel/AdminPanel';
+import UpdateForm from './Component/UpdateForm/UpdateForm';
+import Navbar from './Component/Navbar/Navbar';
+import Login from './Component/Login/Login';
+import Registration from './Component/Registration/Registration';
+import SelectedEvent from './Component/SelectedEvent/SelectedEvent';
+import Home from './Component/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import GlobalValueProvider from './Component/GlobalValue/GlobalValue';
+import PrivateRoute from './Component/PrivateRoute/PrivateRoute';
 
 function App() {
+  const [eventName,setEventName] = useState('');
+  const [img ,setImg] = useState('');
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalValueProvider >
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Navbar></Navbar>
+            <Home setEventName={setEventName} setImg={setImg} ></Home>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/addEvent">
+            <UpdateForm></UpdateForm>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/yourSelectedEvent">
+            <Navbar></Navbar>
+            <SelectedEvent></SelectedEvent>
+          </Route>
+
+          <PrivateRoute path="/selectedEvent">
+            <Registration eventName={eventName} img={img} ></Registration>
+          </PrivateRoute>
+          <Route  path="/admin/alluser">
+            <AdminPanel contro={2}></AdminPanel>
+          </Route>
+          <Route  path="/admin/setevent">
+            <AdminPanel control={1}></AdminPanel>
+          </Route>
+          <Route exact path="/">
+            <Navbar></Navbar>
+            <Home setEventName={setEventName} setImg={setImg}></Home>
+          </Route>
+        
+        </Switch>
+      </Router>
+    </GlobalValueProvider>
+
   );
 }
 
 export default App;
+
